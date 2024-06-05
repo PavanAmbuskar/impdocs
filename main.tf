4a72657aedf24e6382da01d54efa344de0a1ebe5
PavanAmbuskar <166595571+PavanAmbuskar@users.noreply.github.com>
2024-06-05 13:29:18 +0530

    aw

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+provider "aws"  {
+  region = "us-east-1"
+  access_key = "AKIA6ODU4YXHOHYBYIUX"
+  secret_ley = "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"
+}
+resource "aws_instance" "aws1" {
+  ami = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  key_name = "mass"
+}
+
+=======
 provider "aws" "instance" {
   region = "us-east-1"
   access_key = "AKIA6ODU4YXHOHYBYIUX"
@@ -8,3 +21,5 @@ resource "aws_instance" "aws1" {
   instance_type = "t2.micro"
   key_name = "mass"
 }
+
+>>>>>>> 240527cde3c90ea8d6a32d3635bf43786f1c604d
